ee2acca8383d7a63b6ee19b5b33c106a19877de9
Sam A <98753696+samfisherirl@users.noreply.github.com>
2023-12-23 01:53:22 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,1 +1,2 @@
+# assuming your venv is located in /venv/
 pyinstaller --noconfirm --name "QualityScaler"  --onefile --windowed --clean --hidden-import torch --hidden-import torchvision --add-data "\Assets;Assets" --add-binary "\venv\Lib\site-packages\torch_directml\DirectML.dll;torch_directml" --hidden-import cv2 --paths "\venv\Lib\site-packages" "C:\Users\dower\Downloads\QualityScaler-main\QualityScaler.py"
