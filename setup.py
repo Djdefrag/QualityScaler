d707371e5b4cd5d9684731dd958c68f35be7f27f
Sam A <98753696+samfisherirl@users.noreply.github.com>
2023-12-23 02:11:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 # assuming your venv is located in /venv/
-pyinstaller --noconfirm --name "QualityScaler"  --onefile --windowed --clean --hidden-import torch --hidden-import torchvision --add-data "\Assets;Assets" --add-binary "\venv\Lib\site-packages\torch_directml\DirectML.dll;torch_directml" --hidden-import cv2 --paths "\venv\Lib\site-packages" "QualityScaler.py"
+pyinstaller --noconfirm --name "QualityScaler"  --onefile --windowed --clean --hidden-import torch --hidden-import torchvision --add-data "Assets;Assets" --add-data "AI;AI" --add-binary "venv\Lib\site-packages\torch_directml\DirectML.dll;torch_directml" --hidden-import cv2 --paths "venv\Lib\site-packages" "QualityScaler.py"
