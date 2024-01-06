db6a5aeb949518239c6b025c32f81f16cb224f47
zackees <z@zackees.com>
2024-01-06 12:56:15 -0800

    Remove message about python binary

--- src/qualityscaler/cli.py
+++ src/qualityscaler/cli.py
@@ -10,7 +10,6 @@ import sys
 def main() -> int:
     """Main entry point for the template_python_cmd package."""
     python_exe = sys.executable
-    print(f"Python executable: {python_exe}")
     rtn = os.system(f"{python_exe} -m qualityscaler.QualityScaler")
     return rtn
 
