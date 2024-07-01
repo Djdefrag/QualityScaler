f35dc12de8269d2e699ca3d8302a83c624ac140b
Sebastian Olter <qduaty@gmail.com>
2024-07-02 01:15:31 +0200

    minor change for 4096px limiting

--- QualityScaler.py
+++ QualityScaler.py
@@ -1653,11 +1653,9 @@ def upscale_image(
     height, width = get_image_resolution(starting_image)
     height *= resize_factor * upscale_factor
     width *= resize_factor * upscale_factor
-    if height > 4096:
-        resize_factor *= 4096 / height
-        width *= 4096 / height
-    if width > 4096:
-        resize_factor *= 4096 / width
+
+    biggerDim = max(height, width)
+    if biggerDim > 4096: resize_factor *= 4096 / biggerDim
 
     image_to_upscale    = resize_image(starting_image, resize_factor)
     need_tiles          = file_need_tilling(image_to_upscale, tiles_resolution)
